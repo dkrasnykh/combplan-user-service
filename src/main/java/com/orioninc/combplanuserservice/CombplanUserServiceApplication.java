69e72a7fd5c56592adabde71901ee4f2cb936bfd
dkrasnykh <krasnykh.daria@gmail.com>
2020-12-25 15:25:00 +0300

    empty spring boot application

---
+++ src/main/java/com/orioninc/combplanuserservice/CombplanUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.orioninc.combplanuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CombplanUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CombplanUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orioninc/combplanuserservice/CombplanUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orioninc.combplanuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CombplanUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
